f96199bf1b40080d29066596c991f2142957f4c8
David Lyon <dblyon@gmail.com>
2018-06-22 10:54:17 +0200

    added publication URL, bootstrapping and CI for protein level deamidation due to user request

--- README.md
+++ README.md
@@ -6,11 +6,18 @@ The program expects an "evidence.txt" file from <a href="http://www.biochem.mpg.
     - Number_of_Peptides_per_RawFile.txt (number of peptides available for calculation of deamidation for N and for Q)
     - Bootstrapped_values.txt (all the deamidation percentages calculated by e.g. 1000 bootstrap iterations, which are subsequently used to calculate the mean, std, and CI for shown in "Deamidation.txt")
     - Protein_deamidation.txt (deamidation on the protein level, to be used with restraint since there usually are few data to acquire meaningful results, therefore no bootstrapping is applied)
+    - Deamidation on the protein level can also be calculated, although this is NOT recommended as a default analysis (since there often are too little data for this to be meaningful/robust). Use "protein_bootstraps" flag to additionally generate the following files which are analogous the files above but grouped by RawFile and Proteins instead of only by RawFile.
+        Bootstrapped_values_proteins.txt
+        Deamidation_proteins.txt
+        Number_of_Peptides_per_Protein_per_RawFile.txt
 
 The "R_plot.R" script contains code that uses "Deamidation.txt" and "Number_of_Peptides_per_RawFile.txt" to create a BarPlot with STD as error bars, and includes the number of peptides used for the respective deamidation calculation.  
-    
+
 # Citation
-Publication will be referenced here.
+Please cite the following publication when using this tool:
+Mackie et al., Palaeoproteomic Profiling of Conservation Layers on a 14th Century Italian Wall Painting., Angew Chem Int Ed Engl., 2018
+doi:10.1002/anie.201713020
+https://www.ncbi.nlm.nih.gov/pubmed/29603563
 
 # Prerequisites
     - Python 2.x
--- deamidation.py
+++ deamidation.py
@@ -178,7 +178,7 @@ def bootstrap_RFgrouped_avg_Pep(df, mean_or_median="mean"):
     """
     return df[["perc_DeamN", "perc_DeamQ"]].agg(mean_or_median)
 
-def run(fn_evidence, abundance_colname, ci, sampling, num_bootstraps, output_dir, colname_proteins):
+def run(fn_evidence, abundance_colname, ci, sampling, num_bootstraps, output_dir, colname_proteins, protein_bootstraps=False):
     """
     :param fn_evidence: String (absolute path to 'evidence.txt' file)
     :param abundance_colname: String (column name of abundance data e.g. 'Intensity')
@@ -187,6 +187,9 @@ def run(fn_evidence, abundance_colname, ci, sampling, num_bootstraps, output_dir
     :param num_bootstraps: Integer (Number of Bootstrap iterations e.g. 1000)
     :param output_dir: String (absolute path to output directory)
     :param colname_proteins: String (column name of Protein Accession Numbers/Identifierse.g. 'Leading razor protein')
+    :param protein_bootstraps: Boolean(flag to get additional output --> bootstrap Peptides per Protein per RawFile
+        (usually there are not enough data for this to be meaningful and thus this can lead to misleading results,
+        this is NOT recommended as a default analysis)
     :return: None
     """
     ###### use a MaxQuant evidence.txt as input
@@ -207,7 +210,7 @@ def run(fn_evidence, abundance_colname, ci, sampling, num_bootstraps, output_dir
 
     fn_out_num_peptides = os.path.join(output_dir, "Number_of_Peptides_per_RawFile.txt")
     print("Writing {}".format(fn_out_num_peptides))
-    _ = number_of_N_Q_peptides_with_DeamPerc(df, abundance_colname, colname_RawFile, fn_out_num_peptides)
+    _ = number_of_N_Q_peptides_with_DeamPerc(df, colname_RawFile, fn_out_num_peptides)
 
     ### Protein level deamidation --> without CI
     dfp = avg_PSM2Pep(df, colname_RawFile, colname_proteins)
@@ -218,14 +221,32 @@ def run(fn_evidence, abundance_colname, ci, sampling, num_bootstraps, output_dir
 
     print("Bootstrapping")
     fn_out_bootstrapped = os.path.join(output_dir, "Bootstrapped_values.txt")
-    df = deam_per_RawFile_returnBootstrappedVals(df, mqc, colname_proteins, sampling=sampling, num_bootstraps=num_bootstraps, groupby_=colname_RawFile, mean_or_median="mean", average_method="Peptides_per_groupby_")
-    df = df.reset_index(drop=True)
+    df_bs = deam_per_RawFile_returnBootstrappedVals(df, mqc, colname_proteins, sampling=sampling, num_bootstraps=num_bootstraps, groupby_=colname_RawFile, mean_or_median="mean", average_method="Peptides_per_groupby_")
+
+    df_bs = df_bs.reset_index(drop=True)
     print("Writing results to: {}".format(fn_out_bootstrapped))
-    df.to_csv(fn_out_bootstrapped, sep='\t', header=True, index=False)
+    df_bs.to_csv(fn_out_bootstrapped, sep='\t', header=True, index=False)
 
     fn_out = os.path.join(output_dir, "Deamidation.txt")
     print("Writing {}".format(fn_out))
-    calculate_mean_and_CIs(df, ci, colname_RawFile, fn_out)
+    calculate_mean_and_CIs(df_bs, ci, colname_RawFile, fn_out)
+
+    if protein_bootstraps:
+        print("Calculating protein level deamidation per RawFile using bootstraps")
+        df_protein_boots = deam_per_RawFile_returnBootstrappedVals_bootstrapPeptidesPerProtein(df, mqc, colname_proteins=colname_proteins, num_bootstraps=num_bootstraps, groupby_=colname_RawFile)
+        fn_out_bootstrapped_proteins = os.path.join(output_dir, "Bootstrapped_values_proteins.txt")
+        print("Writing results to: {}".format(fn_out_bootstrapped_proteins))
+        df_protein_boots.to_csv(fn_out_bootstrapped_proteins, sep='\t', header=True, index=False)
+
+        fn_out_proteins = os.path.join(output_dir, "Deamidation_proteins.txt")
+        print("Writing {}".format(fn_out_proteins))
+        cond_not_NaN = df_protein_boots["percDeam"].notnull() # need to be removed for CI calculation, but left them in deamidation vals
+        calculate_mean_and_CIs_proteins(df_protein_boots[cond_not_NaN], ci, colname_RawFile, colname_proteins, fn_out_proteins)
+
+        fn_out_num_peptides_proteins = os.path.join(output_dir, "Number_of_Peptides_per_Protein_per_RawFile.txt")
+        print("Writing {}".format(fn_out_num_peptides_proteins))
+        _ = number_of_N_Q_peptides_with_DeamPerc_per_Protein(df, colname_RawFile, colname_proteins, fn_out_num_peptides_proteins)
+
 
 def deam_per_RawFile_returnBootstrappedVals(df, mqc, colname_proteins="Leading razor protein", sampling="peptides", num_bootstraps=1000, groupby_="Raw file", mean_or_median="mean", average_method="Peptides_per_groupby_"):
     """
@@ -302,13 +323,56 @@ def deam_per_RawFile_returnBootstrappedVals(df, mqc, colname_proteins="Leading r
     dfm.loc[dfm["N_Q"] == "perc_DeamQ", "N_Q"] = "Q"
     return dfm
 
-def number_of_N_Q_peptides_with_DeamPerc(df, abundance_colname, colname_RawFile, fn_out):
+def deam_per_RawFile_returnBootstrappedVals_bootstrapPeptidesPerProtein(df, mqc, colname_proteins="Leading razor protein", num_bootstraps=1000, groupby_="Raw file", mean_or_median="mean"):
+    """
+    calculate deamidation-MEAN, but save values for BoxPlot
+    additionally calculate median and add to plot
+
+    first collapse PSMs to Peptides by averaging
+    then bootstrap sampling of Peptides per Proteins --> CAREFUL THIS IS NOT RECOMMENDED AS A DEFAULT METHOD
+    :param df: DataFrame (evidence.txt with deam on PSM level)
+    :param mqc: MQcolnames Instance (check old vs. new colnames in tools-module)
+    :param colname_proteins: String (column name e.g. 'Leading razor protein')
+    :param num_bootstraps: Int
+    :param groupby_: String ('Raw file' or 'Experiment') --> if Experiment names are set
+                properly, then using Experiment will result in useful names for the plot
+    :param mean_or_median: String(Flag to select mean/average or median)
+    :return: DataFrame (Raw File, N_Q, percDeam, CI_low, CI_up)
+    """
+    colname_RawFile = mqc.check_if_colname_or_similar_exists(groupby_)
+    colname_proteins = mqc.check_if_colname_or_similar_exists(colname_proteins)
+    # average PSMs to Peptides
+    dfx = avg_PSM2Pep(df, colname_RawFile, colname_proteins, mean_or_median=mean_or_median)
+    colname_RawFile = mqc.check_if_colname_or_similar_exists(groupby_)
+    grouped = dfx.groupby(colname_RawFile)
+    df_list = []
+    for name, group in grouped:
+        print(name)
+        group_index = group.index.tolist()
+        for random_index in yield_random_combination_with_replacement(group_index, num_bootstraps):
+            dft = dfx.loc[random_index, :]
+            ### sanity check
+            cond = dft.index == random_index
+            assert cond.all() == True
+            df_deam_per_protein = dft.groupby(colname_proteins)[["perc_DeamN", "perc_DeamQ"]].agg(mean_or_median).reset_index()
+            df_deam_per_protein[groupby_] = name
+            df_list.append(df_deam_per_protein)
+    dfm = pd.concat(df_list)
+    dfm = pd.melt(dfm, id_vars=[groupby_, colname_proteins], value_vars=["perc_DeamN", "perc_DeamQ"], var_name="N_Q", value_name="percDeam")
+    dfm.loc[dfm["N_Q"] == "perc_DeamN", "N_Q"] = "N"
+    dfm.loc[dfm["N_Q"] == "perc_DeamQ", "N_Q"] = "Q"
+    return dfm
+
+def number_of_N_Q_peptides_with_DeamPerc(df, colname_RawFile, fn_out):
     """
     number of Peptides for which a deamidation percentage exists, and that can therefore be used for bootstrapping.
     """
     ser_list = []
-    # calc deamidation per RawFile, Sequence and Charge
-    df = df.groupby(by=[colname_RawFile, "Sequence", "Charge"], axis=0).apply(percent_deamidation, abundance_colname)
+
+    # already calculated previously
+    # # calc deamidation per RawFile, Sequence and Charge
+    # df = df.groupby(by=[colname_RawFile, "Sequence", "Charge"], axis=0).apply(percent_deamidation, abundance_colname)
+
     grouped = df[[colname_RawFile, "perc_DeamN", "perc_DeamQ"]].groupby(colname_RawFile)
     for name, group in grouped:
         ser = group[["perc_DeamN", "perc_DeamQ"]].apply(lambda df: sum(df.notnull()))
@@ -317,7 +381,7 @@ def number_of_N_Q_peptides_with_DeamPerc(df, abundance_colname, colname_RawFile,
         ser_list.append(ser)
     df = pd.concat(ser_list, axis=1).T
     df = df.reset_index()
-    df.columns = [[colname_RawFile, "num_N_peptides", "num_Q_peptides"]]
+    df.columns = [colname_RawFile, "num_N_peptides", "num_Q_peptides"]
     dfm = df.melt(id_vars=[colname_RawFile], value_vars=["num_N_peptides", "num_Q_peptides"], var_name="N_Q", value_name="num_peptides")
     cond_n = dfm["N_Q"] == "num_N_peptides"
     dfm.loc[cond_n, "N_Q"] = "N"
@@ -325,6 +389,30 @@ def number_of_N_Q_peptides_with_DeamPerc(df, abundance_colname, colname_RawFile,
     print("Writing results to: {}".format(fn_out))
     dfm.to_csv(fn_out, sep='\t', header=True, index=False)
     return dfm
+
+def number_of_N_Q_peptides_with_DeamPerc_per_Protein(df, colname_RawFile, colname_proteins, fn_out):
+    """
+    number of Peptides for which a deamidation percentage exists, and that can therefore be used for bootstrapping.
+    """
+    ser_list = []
+    grouped = df[[colname_RawFile, colname_proteins, "perc_DeamN", "perc_DeamQ"]].groupby([colname_RawFile, colname_proteins])
+    for name, group in grouped:
+        ser = group[["perc_DeamN", "perc_DeamQ"]].apply(lambda df: sum(df.notnull()))
+        ### doesn't work on groupED object but on group: grouped[["perc_DeamN", "perc_DeamQ"]].apply(lambda df: sum(df.notnull()))
+        ser.name = name
+        ser_list.append(ser)
+    df = pd.concat(ser_list, axis=1).T
+    df = df.reset_index()
+    df.columns = [colname_RawFile, colname_proteins, "num_N_peptides", "num_Q_peptides"]
+    dfm = df.melt(id_vars=[colname_RawFile, colname_proteins], value_vars=["num_N_peptides", "num_Q_peptides"], var_name="N_Q", value_name="num_peptides")
+    cond_n = dfm["N_Q"] == "num_N_peptides"
+    dfm.loc[cond_n, "N_Q"] = "N"
+    dfm.loc[-cond_n, "N_Q"] = "Q"
+    print("Writing results to: {}".format(fn_out))
+    dfm.to_csv(fn_out, sep='\t', header=True, index=False)
+    return dfm
+
+
 
 def calculate_mean_and_CIs(df, ci, groupby_, fn_out):
     ci_low = int((100 - ci) / 2.0)
@@ -340,13 +428,27 @@ def calculate_mean_and_CIs(df, ci, groupby_, fn_out):
     df = pd.merge(dfx, df_ci, how='outer')
     df.to_csv(fn_out, sep='\t', header=True, index=False)
 
+def calculate_mean_and_CIs_proteins(df, ci, groupby_, colname_proteins, fn_out):
+    ci_low = int((100 - ci) / 2.0)
+    ci_up = int(ci + ci_low)
+    percentile_low = df.groupby([groupby_, colname_proteins, "N_Q"])["percDeam"].apply(np.percentile, ci_low).reset_index()
+    percentile_low = percentile_low.rename(columns={"percDeam": "CI_low"})
+    percentile_up = df.groupby([groupby_, colname_proteins, "N_Q"])["percDeam"].apply(np.percentile, ci_up).reset_index()
+    percentile_up = percentile_up.rename(columns={"percDeam": "CI_up"})
+    df_ci = pd.merge(percentile_low, percentile_up, how='outer')
+
+    dfx = df.groupby([groupby_, colname_proteins, "N_Q"])["percDeam"].agg(["mean", "std"]).reset_index()
+    df = pd.merge(dfx, df_ci, how='outer')
+    df.to_csv(fn_out, sep='\t', header=True, index=False)
+    return df
+
 def error_(parser):
     sys.stderr.write("The arguments passed are invalid.\nPlease check the input parameters.\n\n")
     parser.print_help()
     sys.exit(2)
 
 if __name__ == '__main__':
-    cmd_line = True
+    cmd_line = False
 
     if not cmd_line:
         ### input options
@@ -354,9 +456,11 @@ if __name__ == '__main__':
         output_dir = r"/Users/dblyon/Downloads/Output"
         abundance_colname = "Intensity"
         sampling = "peptides"
-        num_bootstraps = 1000
+        num_bootstraps = 10
         ci = 95
         colname_proteins = "Leading razor protein"
+        protein_bootstraps = True
+
 
     else:
         parser = argparse.ArgumentParser()
@@ -367,6 +471,7 @@ if __name__ == '__main__':
         parser.add_argument("-ci", "--confidenceinterval", help="Confidence Interval (default=95)", type=int, default=95)
         parser.add_argument("-b", "--numbootstraps", help="Number of bootstrap iterations (default=1000)", type=int, default=1000)
         parser.add_argument("-c", "--colnameproteins", help="Column name for Protein identifiers (default='Leading razor protein')", type=str, default="Leading razor protein")
+        parser.add_argument("-p", "--proteinbootstraps", help="Bootstrap Peptides per Protein per RawFile. Usually there are not enough data for this to be meaningful and thus, this can lead to misleading results. This is NOT recommended as a default analysis. (default=False)", type=bool, default=False)
 
         args = parser.parse_args()
         fn_evidence = args.evidence
@@ -376,6 +481,7 @@ if __name__ == '__main__':
         num_bootstraps = args.numbootstraps
         output_dir = args.outputdirectory
         colname_proteins = args.colnameproteins
+        protein_bootstraps = args.protein_bootstraps
 
         if fn_evidence is None:
             error_(parser)
@@ -387,5 +493,5 @@ if __name__ == '__main__':
         for arg in sorted(vars(args)):
             print(arg, ": ", getattr(args, arg))
 
-    run(fn_evidence, abundance_colname, ci, sampling, num_bootstraps, output_dir, colname_proteins)
+    run(fn_evidence, abundance_colname, ci, sampling, num_bootstraps, output_dir, colname_proteins, protein_bootstraps)
 
